1d3b4dc8bc1626a5f72393d0b8b9d93e171ab7be
ntibbals <ntibbals@gmail.com>
2019-01-08 19:37:08 -0800

    changed project setup

---
+++ Challenges/Array_Shift/Array_Shift/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_Shift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
