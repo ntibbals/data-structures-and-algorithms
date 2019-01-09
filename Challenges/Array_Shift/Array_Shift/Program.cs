26e8b84b8c5c8a26616ae93d641c23f931899df1
ntibbals <ntibbals@gmail.com>
2019-01-08 20:04:08 -0800

    first mock up based on white board, need to add for loop to display solution in main

--- Challenges/Array_Shift/Array_Shift/Program.cs
+++ Challenges/Array_Shift/Array_Shift/Program.cs
@@ -7,6 +7,27 @@ namespace Array_Shift
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] array = { 1, 2, 3, 4 };
+            int[] solution = (InsertShiftArray(array, 5));
+            Console.ReadLine();
+        }
+
+        static int [] InsertShiftArray(int[] array, int arg)
+        {
+            int[] newArray = new int[array.Length + 1];
+            int variable = array.Length / 2;
+            for( int i = 0; i < array.Length; i++)
+            {
+                if(array[i] == variable)
+                {
+                    newArray[i] = arg;
+                }
+                else
+                {
+                    newArray[i] = array[i];
+                }
+            }
+            return newArray;
         }
     }
 }
