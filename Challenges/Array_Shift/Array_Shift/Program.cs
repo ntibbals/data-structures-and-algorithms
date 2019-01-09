89448b9d9cb9f0470daeb270c07c43b2c9027ccc
ntibbals <ntibbals@gmail.com>
2019-01-08 20:14:23 -0800

    displaying solution, need to refactor method,  is removing/adding incorrectly

--- Challenges/Array_Shift/Array_Shift/Program.cs
+++ Challenges/Array_Shift/Array_Shift/Program.cs
@@ -6,13 +6,19 @@ namespace Array_Shift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Challenge 1 solution is:");
             int[] array = { 1, 2, 3, 4 };
-            int[] solution = (InsertShiftArray(array, 5));
+            int[] solution = (ArrayShift(array, 5));
+            Console.Write("[ ");
+            for (int i = 0; i < solution.Length; i++)
+            {
+                Console.Write($"{String.Join(", ",solution[i])} ");
+            }
+            Console.Write("]");
             Console.ReadLine();
         }
 
-        static int [] InsertShiftArray(int[] array, int arg)
+        static int [] ArrayShift(int[] array, int arg)
         {
             int[] newArray = new int[array.Length + 1];
             int variable = array.Length / 2;
