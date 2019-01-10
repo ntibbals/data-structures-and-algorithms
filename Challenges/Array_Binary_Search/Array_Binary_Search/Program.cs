2a73b227462bc926588025c49c070d17d7247e84
ntibbals <ntibbals@gmail.com>
2019-01-09 20:27:00 -0800

    built out main method with given array and key and created skeleton for search method

--- Challenges/Array_Binary_Search/Array_Binary_Search/Program.cs
+++ Challenges/Array_Binary_Search/Array_Binary_Search/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Array_Binary_Search
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Challenge 2 Solution is: ");
+            int[] array = { 1, 2, 3, 4, 5 };
+            int key = 2;
+            //int solution = BinarySearch(array, key);
+            Console.WriteLine(BinarySearch(array, key));
+            Console.ReadLine();
+        }
+
+        public static int BinarySearch( int[] array, int key)
+        {
+            return 0;
         }
     }
 }
--- Challenges/Array_Binary_Search/README.md
+++ Challenges/Array_Binary_Search/README.md
@@ -3,3 +3,9 @@
 ## Collaborators
 - Nate Tibbals
 - Carlos Roberto Castillo
+
+
+
+## White board layout
+
+![SCREENSHOT](https://github.com/ntibbals/data-structures-and-algorithms/blob/master/assets/array_binary_search.png)
