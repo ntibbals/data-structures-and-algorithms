b61e556f23cbcc5a32e316735ca26b711d98695b
ntibbals <ntibbals@gmail.com>
2019-01-09 20:15:51 -0800

    Initialized project and created readme

---
+++ Challenges/Array_Binary_Search/Array_Binary_Search/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
