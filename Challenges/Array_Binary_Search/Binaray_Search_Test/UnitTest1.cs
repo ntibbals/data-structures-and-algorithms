447e86376546a582b1a61f9133f6a6a156ced4a6
ntibbals <ntibbals@gmail.com>
2019-01-09 21:13:23 -0800

    setup first test, successful

--- Challenges/Array_Binary_Search/Binaray_Search_Test/UnitTest1.cs
+++ Challenges/Array_Binary_Search/Binaray_Search_Test/UnitTest1.cs
@@ -7,9 +7,13 @@ namespace Binaray_Search_Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void FindKey()
         {
-
+            //Test that given a key, it will find the index positino
+            int[] testArray = { 33, 44, 55, 66, 77, 88, 99 };
+            int expectedResult = 4;
+            int testKey = 77;
+            Assert.Equal(expectedResult, Program.BinarySearch(testArray, testKey));
         }
     }
 }
