36c009b4de25c479a32667557dfc674d5b6e28f7
ntibbals <ntibbals@gmail.com>
2019-01-12 22:37:05 -0800

    setup method

---
+++ Challenges/fib_Sequence/fib_Sequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace fib_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int testNum = 4;
+
+            Console.WriteLine(FibSquence(testNum));
+        }
+
+        static int FibSquence(int num)
+        {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
