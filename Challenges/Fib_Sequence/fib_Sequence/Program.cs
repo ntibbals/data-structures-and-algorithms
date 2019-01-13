590812bbe56298ea10e10612642c970b01b94252
ntibbals <ntibbals@gmail.com>
2019-01-12 22:48:49 -0800

    testing, does not work

--- Challenges/Fib_Sequence/fib_Sequence/Program.cs
+++ Challenges/Fib_Sequence/fib_Sequence/Program.cs
@@ -7,6 +7,28 @@ namespace fib_Sequence
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int testNum = 5;
+
+            Console.WriteLine(FibSquence(testNum));
+            Console.ReadLine();
+        }
+
+        static int FibSquence(int num)
+        {
+            int num1;
+            int num2;
+            num1 = 1;
+            num2 = 1;
+            if(num > num2)
+            {
+                while (num2 < num)
+                {
+                    num2 += num1;
+                    num1 = num2 - num1;
+                }
+            }
+
+            return num1;
         }
     }
 }
