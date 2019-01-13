b9318e78ec1e15c52290432aed1268749789a64d
ntibbals <ntibbals@gmail.com>
2019-01-13 13:52:55 -0800

    commented code and updated readme

--- Challenges/Fib_Sequence/fib_Sequence/Program.cs
+++ Challenges/Fib_Sequence/fib_Sequence/Program.cs
@@ -7,19 +7,25 @@ namespace fib_Sequence
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int testNum = 8;
+            int testNum = 8; //integer to pass into method
 
             Console.WriteLine(FibSeq(testNum));
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Fibonacci Sequence method to return nth integer of given argument
+        /// </summary>
+        /// <param name="num">integer to compute</param>
+        /// <returns>nth integer of argument</returns>
         static int FibSeq (int num)
         {
-            int[] fib= new int[num + 1];
+
+            int[] fib= new int[num + 1]; /// initialize array length based on argument plus one
             fib[0] = 0;
             fib[1] = 1;
 
-            for (int i = 2; i <= num; i++)
+            for (int i = 2; i <= num; i++) /// for loop to iterate through array in doing the computations, returns nth integer based on position of argument
             {
                 fib[i] = fib[i-1] + fib[i-2];
             }
