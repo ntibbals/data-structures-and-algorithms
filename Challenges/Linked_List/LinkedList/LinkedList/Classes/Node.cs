6834b96a247d54ec4d01ca8243b7b1a82c26461f
ntibbals <ntibbals@gmail.com>
2019-01-13 15:39:11 -0800

    setup Node class

--- Challenges/Linked_List/LinkedList/LinkedList/Classes/Node.cs
+++ Challenges/Linked_List/LinkedList/LinkedList/Classes/Node.cs
@@ -6,5 +6,12 @@ namespace LinkedList.Classes
 {
     public class Node
     {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
     }
 }
