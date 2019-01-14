e7e1c6de38130990ccef76a5572c33bac2f3242b
ntibbals <ntibbals@gmail.com>
2019-01-13 19:35:38 -0800

    created insert method

--- Challenges/Linked_List/LinkedList/LinkedList/Classes/LinkedList.cs
+++ Challenges/Linked_List/LinkedList/LinkedList/Classes/LinkedList.cs
@@ -7,5 +7,15 @@ namespace LinkedList.Classes
 {
     public class LinkedList
     {
+        public Node Head { get; set; }
+        public Node Current { get; set; }
+
+
+        public void Insert(int value)
+        {
+            Node node1 = new Node(value);
+            node1.Next = Head;
+            Head = node1;
+        }
     }
 }
