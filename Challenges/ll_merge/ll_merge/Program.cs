c8cd8d080f8ff7e0eded93785b6ef49b6be6f3b4
ntibbals <ntibbals@gmail.com>
2019-01-16 22:26:49 -0800

    setup methods

--- Challenges/ll_merge/ll_merge/Program.cs
+++ Challenges/ll_merge/ll_merge/Program.cs
@@ -8,6 +8,23 @@ namespace ll_merge
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            LiList listOne = new LiList();
+            LiList listTwo = new LiList();
+            listOne.Insert(1);
+            listOne.Insert(2);
+            listOne.Insert(3);
+
+            listTwo.Insert(4);
+            listTwo.Insert(5);
+            listTwo.Insert(6);
+            LlMerge(listOne, listTwo);
+            listOne.Print();
+        }
+
+        public static LiList LlMerge(LiList lOne, LiList lTwo)
+        {
+            
+            return lOne;
         }
 
     }
