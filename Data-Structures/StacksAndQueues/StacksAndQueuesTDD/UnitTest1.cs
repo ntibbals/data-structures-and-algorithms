b396e10566df2cd8857ddce7c24fb8227239aee2
ntibbals <ntibbals@gmail.com>
2019-01-22 09:37:31 -0800

    commented testing

--- Data-Structures/StacksAndQueues/StacksAndQueuesTDD/UnitTest1.cs
+++ Data-Structures/StacksAndQueues/StacksAndQueuesTDD/UnitTest1.cs
@@ -9,6 +9,7 @@ namespace StacksAndQueuesTDD
         [Fact]
         public void TestInstantiatesEmptyStack()
         {
+            /// Test instantiation of null stack
             Stack testStack = new Stack();
             Assert.Null(testStack.Top);
 
@@ -17,6 +18,7 @@ namespace StacksAndQueuesTDD
         [Fact]
         public void TestPushIntoEmptyStack()
         {
+            /// Test that push works with an empty stack
             Stack testStack = new Stack();
             int testValue = 3;
             testStack.Push(testValue);
@@ -25,18 +27,19 @@ namespace StacksAndQueuesTDD
         }
 
         [Fact]
-        public void TestPushIntoStackofTwo()
+        public void TestPushIntoSingleStack()
         {
-            int testValue = 11;
+            /// test push into an instantiated stack with a single node
+            Node testNode = new Node(3);
             int testValue2 = 7;
-            Stack testStack = new Stack();
-            testStack.Push(testValue);
+            Stack testStack = new Stack(testNode);
             testStack.Push(testValue2);
             Assert.Equal(testValue2, testStack.Top.Value);
         }
         [Fact]
         public void TestPushMoreThanOne()
         {
+            /// test that multiple instances of push will work back to back
             Node testNode1 = new Node(5);
             int testValue1 = 10;
             int testValue2 = 15;
@@ -48,12 +51,14 @@ namespace StacksAndQueuesTDD
         [Fact]
         public void TestPopOnEmptyStatck()
         {
+            /// Test pop on empty stack
             Stack testStack = new Stack();
             Assert.Throws<NullReferenceException>(() => testStack.Pop());
         }
         [Fact]
         public void TestPopWillRemoveTopValue()
         {
+            /// Test pop will remove top value
             Node testNode1 = new Node(5);
             int testValue1 = 10;
             int testValue2 = 15;
@@ -66,6 +71,7 @@ namespace StacksAndQueuesTDD
         [Fact]
         public void TestPoppingMultipleValues()
         {
+            /// test popping of multiple values back to back
             int testValue = 5;
             int testValue1 = 10;
             int testValue2 = 15;
@@ -78,14 +84,16 @@ namespace StacksAndQueuesTDD
             Assert.Equal(5, testStack.Top.Value);
         }
         [Fact]
-        public void PeekOnEmptyStatck()
+        public void PeekOnEmptyStack()
         {
+            /// test peek on empty stack
             Stack testStack = new Stack();
             Assert.Null(testStack.Peek());
         }
         [Fact]
-        public void PeekOnStatckOfOne()
+        public void PeekOnStackOfOne()
         {
+            /// test peek on stack of One
             Node testNode = new Node(5);
             Stack testStack = new Stack(testNode);
             testStack.Push(10);
@@ -95,6 +103,7 @@ namespace StacksAndQueuesTDD
         [Fact]
         public void PeekWillNotAlterStack()
         {
+            /// That that peek will not remove or alter the existing stack
             Node testNode = new Node(5);
             Stack testStack = new Stack(testNode);
             testStack.Push(10);
@@ -104,13 +113,15 @@ namespace StacksAndQueuesTDD
         [Fact]
         public void TestInstantiatesEmptyQueue()
         {
+            /// test instantiation of empty queue
             Queue testQueue = new Queue();
-            Assert.Null(testQueue.Front);
+            Assert.Null(testQueue.Rear);
 
         }
         [Fact]
         public void TestEnqueRearValueOfQueue()
         {
+            /// test enque will add value to end of queue(rear)
             Node testNode = new Node(3);
             Queue testQueue = new Queue(testNode);
             int testValue = 7;
@@ -119,8 +130,9 @@ namespace StacksAndQueuesTDD
             Assert.Equal(expectedValue, testQueue.Rear.Value);
         }
         [Fact]
-        public void TestEnqueFrontValueOfQueue()
+        public void TestEnqueueFrontValueOfQueue()
         {
+            /// test enqueue will not alter front value of queue
             Node testNode = new Node(11);
             Queue testQueue = new Queue(testNode);
             int testValue = 13;
@@ -129,6 +141,70 @@ namespace StacksAndQueuesTDD
             Assert.Equal(expectedValue, testQueue.Front.Value);
         }
 
+        [Fact]
+        public void TestDequeueOnEmptyQueue()
+        {
+            /// test dequeue on empty queue
+            Queue testQueue = new Queue(); 
+            Assert.Throws<NullReferenceException>(() => testQueue.Dequeue());
+        }
 
+        [Fact]
+        public void TestDenqueueWillRemoveFrontOfQueue()
+        {
+            /// test dequeue will remove front of queue
+            Node testNode = new Node(11);
+            Queue testQueue = new Queue(testNode);
+            int testValue = 13;
+            testQueue.Enqueue(testValue);
+            testQueue.Dequeue();
+            int expectedValue = 13;
+            Assert.Equal(expectedValue, testQueue.Rear.Value);
+        }
+        [Fact]
+        public void TestDenqueueOnMultipleValues()
+        {
+            /// test dequeue on multiple values back to back
+            Node testNode = new Node(5);
+            Queue testQueue = new Queue(testNode);
+            int testValue = 10;
+            int testValue2 = 15;
+            testQueue.Enqueue(testValue);
+            testQueue.Enqueue(testValue2);
+            testQueue.Dequeue();
+            testQueue.Dequeue();
+            int expectedValue = 15;
+            Assert.Equal(expectedValue, testQueue.Rear.Value);
+        }
+
+        [Fact]
+        public void PeekOnEmptyQueue()
+        {
+            /// test peek on empty queue
+            Queue testQueue = new Queue();
+            Assert.Null(testQueue.Peek());
+        }
+        [Fact]
+        public void PeekOnQueueOfOne()
+        {
+            /// test peek on queue of one
+            Node testNode = new Node(5);
+            Queue testQueue = new Queue(testNode);
+            int testValue = 10;
+            int testValue2 = 15;
+            testQueue.Enqueue(testValue);
+            testQueue.Enqueue(testValue2);
+            Assert.Equal(testQueue.Front, testQueue.Peek());
+        }
+        [Fact]
+        public void PeekWillNotAlterQueue()
+        {
+            /// test peek will not alter/remove value in queue
+            Node testNode = new Node(15);
+            Queue testQueue = new Queue(testNode);
+            testQueue.Enqueue(20);
+            testQueue.Peek();
+            Assert.Equal(20, testQueue.Rear.Value);
+        }
     }
 }
