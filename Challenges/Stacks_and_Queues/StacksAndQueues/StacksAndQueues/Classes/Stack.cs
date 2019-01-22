271f41b207672d0731420115e0ed1d19e256af88
ntibbals <ntibbals@gmail.com>
2019-01-21 19:08:03 -0800

    setup statck Top and Push

--- Challenges/Stacks_and_Queues/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
+++ Challenges/Stacks_and_Queues/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -6,5 +6,19 @@ namespace StacksAndQueues.Classes
 {
     class Stack
     {
+
+        public Node Top { get; set; }
+
+        public Stack(Node node)
+        {
+
+        }
+
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            node.Next = Top;
+            Top = node;
+        }
     }
 }
