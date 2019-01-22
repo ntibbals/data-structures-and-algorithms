728dbd91dca2ef4c4b38cff3d836d055c59389da
ntibbals <ntibbals@gmail.com>
2019-01-21 19:12:02 -0800

    setup Que class with front, rear and size

---
+++ Challenges/Stacks_and_Queues/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    public class Queue
+    {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        public int Size = 0;
+
+    }
+}
