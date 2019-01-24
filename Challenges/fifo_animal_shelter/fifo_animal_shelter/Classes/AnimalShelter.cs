c4495b20d9afcedf2db834a175476f867b499fe9
ntibbals <ntibbals@gmail.com>
2019-01-23 22:34:32 -0800

    created class

---
+++ Challenges/fifo_animal_shelter/fifo_animal_shelter/Classes/AnimalShelter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StacksAndQueues.Classes;
+
+namespace fifo_animal_shelter.Classes
+{
+    class AnimalShelter
+    {
+        public string Type { get; set; }
+
+        public AnimalShelter(string type)
+        {
+            Type = type;
+        }
+        public void Enqueue(string animal)
+        {
+            if(animal.Equals("dog") || animal.Equals("cat"))
+            {
+                Type = animal;
+            }
+        }
+    }
+}
