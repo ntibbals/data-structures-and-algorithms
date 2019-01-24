a3fda2e812b81a597228a9627270169e5db670eb
ntibbals <ntibbals@gmail.com>
2019-01-23 22:58:15 -0800

    mock write up on dequeue

--- Challenges/fifo_animal_shelter/fifo_animal_shelter/Classes/AnimalShelter.cs
+++ Challenges/fifo_animal_shelter/fifo_animal_shelter/Classes/AnimalShelter.cs
@@ -7,17 +7,74 @@ namespace fifo_animal_shelter.Classes
 {
     class AnimalShelter
     {
-        public string Type { get; set; }
+        public string Dog { get; set; }
+        public string Cat { get; set; }
+        public Queue Shelter { get; set; }
 
         public AnimalShelter(string type)
         {
-            Type = type;
+            Dog = type;
+            Cat = type;
+            Shelter = new Queue();
         }
         public void Enqueue(string animal)
         {
-            if(animal.Equals("dog") || animal.Equals("cat"))
+            if (animal.Equals("dog"))
             {
-                Type = animal;
+                Shelter.Enqueue(1);
+            }
+            else if (animal.Equals("cat"))
+            {
+                Shelter.Enqueue(2);
+            }
+            else
+            {
+                Console.WriteLine("Wrong Shelter");
+            }
+        }
+
+        public string Dequeue(string pref)
+        {
+            if (pref.Equals("dog") || pref.Equals("cat"))
+            {
+                if (Shelter.Front.Value == 1 || Shelter.Front.Value == 2)
+                {
+                    Node temp = Shelter.Front;
+                    Shelter.Front = Shelter.Front.Next;
+                    if (temp.Value == 1)
+                    {
+                        return "dog";
+                    }
+                    else
+                    {
+                        return "cat";
+                    }
+                }
+                else
+                {
+                    while (Shelter.Front.Value != 1 || Shelter.Front.Value != 2)
+                    {
+                        Node temp = Shelter.Front;
+                        Shelter.Front = Shelter.Front.Next;
+                        Shelter.Rear.Next = temp;
+                        Shelter.Rear = temp;
+                    }
+                    Node temp2 = Shelter.Front;
+                    Shelter.Front = Shelter.Front.Next;
+                    if (temp2.Value == 1)
+                    {
+                        return "dog";
+                    }
+                    else
+                    {
+                        return "cat";
+                    }
+                }
+
+            }
+            else
+            {
+                return null;
             }
         }
     }
