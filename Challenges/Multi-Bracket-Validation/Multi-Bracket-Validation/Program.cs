90a4dfca52967673530a9d570a8ab9342a794480
ntibbals <ntibbals@gmail.com>
2019-01-24 20:00:23 -0800

    working on method build out

--- Challenges/Multi-Bracket-Validation/Multi-Bracket-Validation/Program.cs
+++ Challenges/Multi-Bracket-Validation/Multi-Bracket-Validation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Multi_Bracket_Validation.Classes;
 
 namespace Multi_Bracket_Validation
 {
@@ -13,8 +14,29 @@ namespace Multi_Bracket_Validation
 
         public static bool MultiBracketValidation(string input)
         {
+            char[] testArray = new char[input.Length];
+            Stack stack = new Stack();
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                testArray[i] = input[i];
+            }
 
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                if(testArray[i] == '{')
+                {
+                    stack.Push(testArray[i]);
+                }
+                else if (testArray[i] == '(')
+                {
+                    stack.Push(testArray[i]);
+                }
+                else if (testArray[i] == '[')
+                {
+                    stack.Push(testArray[i]);
+                }
 
+            }
 
             return true;
         }
