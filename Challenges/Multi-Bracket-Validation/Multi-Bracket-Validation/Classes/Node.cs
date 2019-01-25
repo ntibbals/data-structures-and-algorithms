1acea5e297f47885cff1fb644de9f6c499a1ee4a
ntibbals <ntibbals@gmail.com>
2019-01-24 19:39:38 -0800

    created node and stack classes

---
+++ Challenges/Multi-Bracket-Validation/Multi-Bracket-Validation/Classes/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multi_Bracket_Validation.Classes
+{
+    class Node
+    {
+        /// <summary>
+        /// Sets Node property for value
+        /// </summary>
+        public int Value { get; set; }
+        /// <summary>
+        /// set Node property for next
+        /// </summary>
+        public Node Next { get; set; }
+
+        /// <summary>
+        /// Constructor for node
+        /// </summary>
+        /// <param name="value">integer value</param>
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
