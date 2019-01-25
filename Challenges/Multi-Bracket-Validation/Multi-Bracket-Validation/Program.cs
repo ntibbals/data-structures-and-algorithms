c6e232a4c1177a344af351ee0d8b58228ca250fe
ntibbals <ntibbals@gmail.com>
2019-01-24 19:41:27 -0800

    established new method in main

--- Challenges/Multi-Bracket-Validation/Multi-Bracket-Validation/Program.cs
+++ Challenges/Multi-Bracket-Validation/Multi-Bracket-Validation/Program.cs
@@ -7,6 +7,16 @@ namespace Multi_Bracket_Validation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string input = "{{fa}";
+            MultiBracketValidation(input);
+        }
+
+        public static bool MultiBracketValidation(string input)
+        {
+
+
+
+            return true;
         }
     }
 }
