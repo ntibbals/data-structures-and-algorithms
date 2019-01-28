f827115395185b5c05b735f5c081bd1b741771fb
ntibbals <ntibbals@gmail.com>
2019-01-27 21:05:40 -0800

    setup pre order

--- Data-Structures/Trees/Trees/Classes/BinaryTree.cs
+++ Data-Structures/Trees/Trees/Classes/BinaryTree.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Trees.Classes;
 
 namespace Trees.Classes
 {
     class BinaryTree
     {
+
+        public Node Root;
+
+        public bool IsEmpty
+        {
+            get { return Root == null;  }
+        }
+
+        public int Count { get; private set; }
+
+        public BinaryTree()
+        {
+            Root = null;
+            Count = 0;
+        }
+
+        public void  PreOrder(Node root)
+        {
+            if (root != null)
+            {
+                Console.Write(Root.Value + " ");
+                PreOrder(root.Left);
+                PreOrder(root.Right);
+                return;
+            }
+        }
     }
 }
--- Data-Structures/Trees/Trees/Classes/Node.cs
+++ Data-Structures/Trees/Trees/Classes/Node.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace Trees.Classes
 {
-    class Node
+    public class Node
     {
+        /// <summary>
+        /// Sets Node property for value
+        /// </summary>
+        public int Value { get; set; }
+        /// <summary>
+        /// set Node property for left child
+        /// </summary>
+        public Node Left { get; set; }
+        /// <summary>
+        /// set Node property for right child
+        /// </summary>
+        public Node Right{ get; set; }
+
+        /// <summary>
+        /// Constructor for node
+        /// </summary>
+        /// <param name="value">integer value</param>
+        public Node(int value)
+        {
+            Value = value;
+        }
     }
 }
