f8aa5a9f0a9f62ef28a9cfd82faff71a3370d314
ntibbals <ntibbals@gmail.com>
2019-01-29 23:05:49 -0800

    setup project, node class, queue class

---
+++ Challenges/BreadFirstTraversal/BreadFirstTraversal/Classes/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadFirstTraversal.Classes
+{
+    public class Node
+    {
+        /// <summary>
+        /// Sets Node property for value
+        /// </summary>
+        public int Value { get; set; }
+        /// <summary>
+        /// set Node property for next
+        /// </summary>
+        public Node Next { get; set; }
+
+        /// <summary>
+        /// Constructor for node
+        /// </summary>
+        /// <param name="value">integer value</param>
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
