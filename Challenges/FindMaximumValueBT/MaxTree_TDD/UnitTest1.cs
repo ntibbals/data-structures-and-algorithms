9dc4ac6a6adca455a07243f94c706d5a21cfdc13
ntibbals <ntibbals@gmail.com>
2019-01-31 10:32:15 -0800

    all tests passing

--- Challenges/FindMaximumValueBT/MaxTree_TDD/UnitTest1.cs
+++ Challenges/FindMaximumValueBT/MaxTree_TDD/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace MaxTree_TDD
         [Fact]
         public void TestMaxOnLastBranch()
         {
+            /// Test when maximum value is right most branch
             BT testTree = new BT();
             testTree.Root = new Nodeb(100);
             Nodeb leftChild = new Nodeb(5);
@@ -19,9 +20,52 @@ namespace MaxTree_TDD
             testTree.Root.Right = rightChild;
 
             Nodeb leftRightLeaf = new Nodeb(200);
-            testTree.Root.Left.Right = leftRightLeaf;
+            testTree.Root.Right.Right = leftRightLeaf;
 
             Assert.Equal(200, Program.FindMaxValue(testTree));
+        }
+
+        [Fact]
+        public void TestMaxOnMiddleBranch()
+        {
+            /// Test when maximum value is left most branch
+            BT testTree = new BT();
+            testTree.Root = new Nodeb(100);
+            Nodeb leftChild = new Nodeb(5);
+            testTree.Root.Left = leftChild;
+
+            Nodeb rightChild = new Nodeb(10);
+            testTree.Root.Right = rightChild;
+
+            Nodeb leftRightLeaf = new Nodeb(130);
+            testTree.Root.Left.Left = leftRightLeaf;
+
+            Assert.Equal(130, Program.FindMaxValue(testTree));
+        }
+        [Fact]
+        public void TestMaxOnRoot()
+        {
+            /// Test when maximum value is root
+            BT testTree = new BT();
+            testTree.Root = new Nodeb(200);
+            Nodeb leftChild = new Nodeb(5);
+            testTree.Root.Left = leftChild;
+
+            Nodeb rightChild = new Nodeb(10);
+            testTree.Root.Right = rightChild;
+
+            Nodeb leftRightLeaf = new Nodeb(130);
+            testTree.Root.Left.Left = leftRightLeaf;
+
+            Assert.Equal(200, Program.FindMaxValue(testTree));
+        }
+        [Fact]
+        public void TestMaxOnEmptyTree()
+        {
+            /// Test when maximum value is root
+            BT testTree = new BT();
+             
+            Assert.Throws<NullReferenceException>(() =>Program.FindMaxValue(testTree));
         }
     }
 }
