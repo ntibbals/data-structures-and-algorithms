e2b3fee83d1f81ec4799fb33c8710c015f0dea1f
ntibbals <ntibbals@gmail.com>
2019-02-11 15:13:03 -0800

    working solution

---
+++ Challenges/Are_Characters_Unique/Are_Characters_Unique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Are_Characters_Unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string inpu = "abcadef";
+            Console.WriteLine(AreCharactersUnique(inpu));
+        }
+
+        static bool AreCharactersUnique (string input)
+        {
+            string[] array = new string [input.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = input[i].ToString();
+            }
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i+ 1; j < array.Length - 1; j++)
+                {
+                    Console.WriteLine($"{array[i]} and {array[j + 1]}");
+                    if (array[i] == array[j + 1])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+    }
+}
