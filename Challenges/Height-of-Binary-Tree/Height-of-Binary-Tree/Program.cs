b14fc01f8a540bece2ce7c2dedb4109481df3fef
ntibbals <ntibbals@gmail.com>
2019-02-11 10:34:13 -0800

    built out solution, working on testing

--- Challenges/Height-of-Binary-Tree/Height-of-Binary-Tree/Program.cs
+++ Challenges/Height-of-Binary-Tree/Height-of-Binary-Tree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Trees.Classes;
 
 namespace Height_of_Binary_Tree
 {
@@ -7,6 +8,60 @@ namespace Height_of_Binary_Tree
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BT tree = new BT();
+            tree.Root = new Nodeb(100);
+
+            Nodeb leftChild = new Nodeb(5);
+            tree.Root.Left = leftChild;
+
+            Nodeb rightChild = new Nodeb(10);
+            tree.Root.Right = rightChild;
+
+            Nodeb leftLeftLeaf = new Nodeb(15);
+            tree.Root.Left.Left = leftLeftLeaf;
+
+            Nodeb leftRightLeaf = new Nodeb(200);
+            tree.Root.Left.Right = leftRightLeaf;
+
+            Nodeb rightLeftLeaf = new Nodeb(30);
+            tree.Root.Right.Left = rightLeftLeaf;
+
+            Nodeb rightRightLeaf = new Nodeb(150);
+            tree.Root.Right.Right = rightRightLeaf;
+            Nodeb rightRightRightLeaf = new Nodeb(160);
+            tree.Root.Right.Right.Right = rightRightRightLeaf;
+
+            Console.WriteLine(FindHeight(tree));
+            Console.ReadLine();
+
+        }
+
+        public static int FindHeight(BT tree)
+        {
+            if (tree.Root == null)
+            {
+                throw null;
+            }
+
+            Nodeb root = tree.Root;
+            int currentCount = 0;
+
+            void Helper(Nodeb temp)
+            {
+                if (temp == null)
+                {
+                    return;
+                }
+                Helper(temp.Left);
+                Helper(temp.Right);
+                if( temp.Left != null || temp.Right != null)
+                {
+                    currentCount++;
+                }
+            }
+            Helper(root);
+
+            return currentCount;
         }
     }
 }
