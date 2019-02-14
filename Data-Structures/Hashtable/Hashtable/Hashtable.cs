464d55ba038cf510fc451e380d83b950684c0963
ntibbals <ntibbals@gmail.com>
2019-02-13 19:13:13 -0800

    built out pointer for dual inputs

---
+++ Data-Structures/Hashtable/Hashtable/Hashtable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtable
+{
+    class Hashtable
+    {
+        public struct Point
+        {
+            private int a;
+            private int b;
+
+            public Point(int a, int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Point)) return false;
+                Point point = (Point)obj;
+                return a == point.a & b == point.b;
+            }
+
+            public override int GetHashCode()
+            {
+                return ShiftAndWrap(a.GetHashCode(), 2) ^ b.GetHashCode();
+            }
+
+            private int ShiftAndWrap(int value, int positions)
+            {
+                positions = positions & 0x1F; // represents 32 bit integer
+
+                uint num = BitConverter.ToUInt32(BitConverter.GetBytes(value), 0);
+                uint wrapped = num >> (32 - positions);
+
+                return BitConverter.ToInt32(BitConverter.GetBytes((num << positions) | wrapped), 0);
+            }
+        }
+
+    }
+}
--- Data-Structures/Hashtable/Hashtable/Program.cs
+++ Data-Structures/Hashtable/Hashtable/Program.cs
@@ -7,6 +7,8 @@ namespace Hashtable
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(GetHashCode(5));
         }
     }
 }
