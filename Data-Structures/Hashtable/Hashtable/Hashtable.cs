ba77e4f07d3fb5e4756c23c024bbf7d4a6c36147
ntibbals <ntibbals@gmail.com>
2019-02-13 19:52:23 -0800

    setup Hashtable class

--- Data-Structures/Hashtable/Hashtable/Hashtable.cs
+++ Data-Structures/Hashtable/Hashtable/Hashtable.cs
@@ -6,6 +6,23 @@ namespace Hashtable
 {
     public class Hashtable
     {
- 
+        public NodeH[] Table { get; set; }
+
+        public int Buckets { get; set; }
+
+
+        public Hashtable(int buckets)
+        {
+            Buckets = buckets;
+            Table = new NodeH[Buckets];
+        }
+
+        public int GetKey(int key)
+        {
+
+
+
+            return key;
+        }
     }
 }
