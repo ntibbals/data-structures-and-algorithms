ac47261b8cc717598f7a0d1526b409a587041a8e
ntibbals <ntibbals@gmail.com>
2019-02-14 20:16:01 -0800

    sorted

--- Challenges/sorts/Insertion_Sort/Insertion_Sort/Program.cs
+++ Challenges/sorts/Insertion_Sort/Insertion_Sort/Program.cs
@@ -7,6 +7,30 @@ namespace Insertion_Sort
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] sortArray = new int[] { 1, 9, 5, 4, 2 };
+            int[] sorted = InsertionSort(sortArray);
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                Console.Write($" {sorted[i]} ,");
+            }
+            Console.ReadLine();
+
+        }
+
+        public static int[] InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int x = i - 1;
+                int temp = array[i];
+                while( x >= 0 && temp < array[x] )
+                {
+                    array[x + 1] = array[x];
+                    x = x - 1;
+                }
+                array[x + 1] = temp;
+            }
+            return array;
         }
     }
 }
