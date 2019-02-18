da635315e4275531db535a80e59c36c53913af1b
ntibbals <ntibbals@gmail.com>
2019-02-17 21:15:03 -0800

    testing success

--- Challenges/sorts/Insertion_Sort/InsertSort_TDD/UnitTest1.cs
+++ Challenges/sorts/Insertion_Sort/InsertSort_TDD/UnitTest1.cs
@@ -9,8 +9,50 @@ namespace InsertSort_TDD
         [Fact]
         public void CanSortArrayofTwo()
         {
+            // Sort array with two index
             int[] testArray = new int[] { 9, 1 };
             int[] expected = new int[] { 1, 9 };
+
+            Assert.Equal(expected, Program.InsertionSort(testArray));
+
+        }
+
+        [Fact]
+        public void CanSortArrayofFive()
+        {
+            // sort array with length of five
+            int[] testArray = new int[] { 1, 9, 5, 4, 2 };
+            int[] expected = new int[] { 1, 2, 4, 5, 9 };
+
+            Assert.Equal(expected, Program.InsertionSort(testArray));
+
+        }
+        [Fact]
+        public void CanSortWithDuplicates()
+        {
+            // can sort with multiple duplicates
+            int[] testArray = new int[] { 9, 9, 5, 2, 2 };
+            int[] expected = new int[] { 2, 2, 5, 9, 9 };
+
+            Assert.Equal(expected, Program.InsertionSort(testArray));
+
+        }
+        [Fact]
+        public void CanSortWithEmptyArray()
+        {
+            // can sort an empty array
+            int[] testArray = new int[] { };
+            int[] expected = new int[] { };
+
+            Assert.Equal(expected, Program.InsertionSort(testArray));
+
+        }
+        [Fact]
+        public void CanSortNegativeIntegers()
+        {
+            // can sort negative integers
+            int[] testArray = new int[] { 1, 9, -5, 4, 2 };
+            int[] expected = new int[] { -5, 1, 2, 4, 9 };
 
             Assert.Equal(expected, Program.InsertionSort(testArray));
 
