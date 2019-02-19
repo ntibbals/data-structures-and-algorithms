ce0f3d5c867acd2f6bc8f29229d0610cdaa06cbc
ntibbals <ntibbals@gmail.com>
2019-02-19 14:01:40 -0800

    commented code

--- Challenges/Tree_Intersection/Tree_Intersection/Program.cs
+++ Challenges/Tree_Intersection/Tree_Intersection/Program.cs
@@ -12,6 +12,12 @@ namespace Tree_Intersection
             Console.WriteLine("Hello World!");
         }
 
+        /// <summary>
+        /// Identify pairs between two binary trees
+        /// </summary>
+        /// <param name="treeOne">binary tree one</param>
+        /// <param name="treeTwo">binary tree two</param>
+        /// <returns>list of pair values between both trees</returns>
         public static List<int> TreeIntersection( BT treeOne, BT treeTwo)
         {
             List<int> values = new List<int>();
@@ -20,6 +26,7 @@ namespace Tree_Intersection
             Nodeb root1 = treeOne.Root;
             Nodeb root2 = treeTwo.Root;
 
+            /// first helper method to traverse first tree adding to hash table
             void TraverseOne(Nodeb helperRoot)
             {
                 if(helperRoot == null)
@@ -32,6 +39,7 @@ namespace Tree_Intersection
                 table.Add(helperRoot.Value.ToString(), helperRoot.Value);
             }
             TraverseOne(root1);
+            ///second helper method to traverse second tree to cross reference table
             void TraverseTwo(Nodeb helperRoot)
             {
                 if (helperRoot == null)
--- Challenges/Tree_Intersection/tree_intersection_tdd/UnitTest1.cs
+++ Challenges/Tree_Intersection/tree_intersection_tdd/UnitTest1.cs
@@ -12,6 +12,7 @@ namespace tree_intersection_tdd
         [Fact]
         public void FindTwoIntersections()
         {
+            /// Find two pairs between both trees
             BT testTree = new BT();
             testTree.Root = new Nodeb(100);
             Nodeb leftChild = new Nodeb(5);
@@ -41,6 +42,7 @@ namespace tree_intersection_tdd
         [Fact]
         public void FindRootIntersection()
         {
+            /// Find root pairs
             BT testTree = new BT();
             testTree.Root = new Nodeb(100);
             Nodeb leftChild = new Nodeb(2);
@@ -63,6 +65,47 @@ namespace tree_intersection_tdd
             Nodeb leftRightLeaf2 = new Nodeb(20);
             testTree2.Root.Left.Right = leftRightLeaf2;
             List<int> expected = new List<int> { 100 };
+            Assert.Equal(expected, Program.TreeIntersection(testTree, testTree2));
+
+        }
+        [Fact]
+        public void FindEmptyTreeIntersection()
+        {
+            /// Find intersection on empty list
+            BT testTree = new BT();
+
+            BT testTree2 = new BT();
+
+            List<int> expected = new List<int> { };
+            Assert.Equal(expected, Program.TreeIntersection(testTree, testTree2));
+
+        }
+        [Fact]
+        public void FindNoIntersection()
+        {
+            /// Look for intersection where none exist
+            BT testTree = new BT();
+            testTree.Root = new Nodeb(99);
+            Nodeb leftChild = new Nodeb(2);
+            testTree.Root.Left = leftChild;
+
+            Nodeb rightChild = new Nodeb(10);
+            testTree.Root.Right = rightChild;
+
+            Nodeb leftRightLeaf = new Nodeb(21);
+            testTree.Root.Left.Right = leftRightLeaf;
+
+            BT testTree2 = new BT();
+            testTree2.Root = new Nodeb(100);
+            Nodeb leftChild2 = new Nodeb(5);
+            testTree2.Root.Left = leftChild2;
+
+            Nodeb rightChild2 = new Nodeb(15);
+            testTree2.Root.Right = rightChild2;
+
+            Nodeb leftRightLeaf2 = new Nodeb(20);
+            testTree2.Root.Left.Right = leftRightLeaf2;
+            List<int> expected = new List<int> { };
             Assert.Equal(expected, Program.TreeIntersection(testTree, testTree2));
 
         }
