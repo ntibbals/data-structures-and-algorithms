97f22f060fb77087f2f2533363421b839d2d1dc7
ntibbals <ntibbals@gmail.com>
2019-02-18 21:54:26 -0800

    comments

--- Challenges/Repeated_Word/Repeated_Word/Program.cs
+++ Challenges/Repeated_Word/Repeated_Word/Program.cs
@@ -12,23 +12,28 @@ namespace Repeated_Word
             Console.WriteLine(RepeatedWord(coco));
         }
 
+        /// <summary>
+        /// Method that identifies first repeated word in string
+        /// </summary>
+        /// <param name="phrase">lengthy string</param>
+        /// <returns>first repeated word or the entire string if none is found</returns>
         public static string RepeatedWord(string phrase)
         {
-            string[] array = phrase.ToLower().Split(" ");
+            string[] array = phrase.ToLower().Split(" "); /// split into array
 
-            Hashtable words = new Hashtable(100);
+            Hashtable words = new Hashtable(100); ///instantiate new hash table to hold values
             for (int i = 0; i < array.Length; i++)
             {
                 if(words.Contains(array[i]))
                 {
-                    return array[i];
+                    return array[i]; /// return value if contained in hash table
                 }
                 else
                 {
-                    words.Add(array[i], array[i]);
+                    words.Add(array[i], array[i]); /// else add index value as key and value
                 }
             }
-            return phrase;
+            return phrase; /// if no repeated word is found, return phrase
         }
 
 
