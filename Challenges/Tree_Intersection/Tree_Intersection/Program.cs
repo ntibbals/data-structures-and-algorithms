d6360b39bc59f2649bf63aff61852adf22f2b9b1
ntibbals <ntibbals@gmail.com>
2019-02-19 13:27:13 -0800

    setup first test and method skeleton

--- Challenges/Tree_Intersection/Tree_Intersection/Program.cs
+++ Challenges/Tree_Intersection/Tree_Intersection/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using Hashtables.Classes;
 using Trees.Classes;
 
 namespace Tree_Intersection
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static List<int> TreeIntersection( BT treeOne, BT treeTwo)
+        {
+            List<int> values = new List<int>();
+
+
+
+            return values;
         }
     }
 }
--- Challenges/Tree_Intersection/tree_intersection_tdd/UnitTest1.cs
+++ Challenges/Tree_Intersection/tree_intersection_tdd/UnitTest1.cs
@@ -3,14 +3,38 @@ using Xunit;
 using Hashtables.Classes;
 using Trees.Classes;
 using Tree_Intersection;
+using System.Collections.Generic;
 
 namespace tree_intersection_tdd
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void FindTwoIntersections()
         {
+            BT testTree = new BT();
+            testTree.Root = new Nodeb(100);
+            Nodeb leftChild = new Nodeb(5);
+            testTree.Root.Left = leftChild;
+
+            Nodeb rightChild = new Nodeb(10);
+            testTree.Root.Right = rightChild;
+
+            Nodeb leftRightLeaf = new Nodeb(20);
+            testTree.Root.Left.Right = leftRightLeaf;
+
+            BT testTree2 = new BT();
+            testTree2.Root = new Nodeb(99);
+            Nodeb leftChild2 = new Nodeb(5);
+            testTree2.Root.Left = leftChild2;
+
+            Nodeb rightChild2 = new Nodeb(15);
+            testTree2.Root.Right = rightChild2;
+
+            Nodeb leftRightLeaf2 = new Nodeb(20);
+            testTree2.Root.Left.Right = leftRightLeaf2;
+            List<int> expected = new List<int> { 5, 20 };
+            Assert.Equal(expected, Program.TreeIntersection(testTree, testTree2));
 
         }
     }
