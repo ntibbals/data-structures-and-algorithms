f894da671e8d983d290ad9fe3d9dfa2a837613c9
ntibbals <ntibbals@gmail.com>
2019-02-24 12:55:15 -0800

    built out add methods for graph

--- Data-Structures/Graph/Graph/Classes/Graph.cs
+++ Data-Structures/Graph/Graph/Classes/Graph.cs
@@ -19,5 +19,32 @@ namespace Graph.Classes
                 v.Graph = this;
             }
         }
+
+        public void AddEdge(Edge edge)
+        {
+            Edges.Add(edge);
+        }
+
+        public void AddEdge(Vertex v1, Vertex v2, int weight)
+        {
+            Edges.Add(new Edge(v1, v2, weight));
+        }
+
+        public void AddVertex(Vertex vertices)
+        {
+            Vertices.Add(vertices);
+            vertices.Graph = this;
+        }
+
+        public void RemoveEdge(Edge edge)
+        {
+            Edges.Remove(edge);
+        }
+
+        public void RemoveVertex(Vertex vertices)
+        {
+            Edges.RemoveAll(edge => edge.V2 == vertices || edge.V1 == vertices);
+            Vertices.Remove(vertices);
+        }
     }
 }
