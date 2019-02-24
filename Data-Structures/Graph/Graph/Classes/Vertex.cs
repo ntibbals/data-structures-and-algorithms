eabb13b3378195a69ea2b63bbdf0af5ed5fa4e15
ntibbals <ntibbals@gmail.com>
2019-02-24 11:59:22 -0800

    added vertex(node)

---
+++ Data-Structures/Graph/Graph/Classes/Vertex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph.Classes
+{
+    class Vertex
+    {
+        public object Value { get; set; }
+
+        public Vertex(object value)
+        {
+            Value = value;
+        }
+    }
+}
