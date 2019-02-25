a1ab1bd1a9382e029576fece275d4f80970639c0
ntibbals <ntibbals@gmail.com>
2019-02-24 18:45:28 -0800

    added node hash table class

---
+++ Challenges/Common_Word/Common_Word/Classes/HashT.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common_Word.Classes
+{
+    class HashT
+    {
+    }
+}
---
+++ Challenges/Common_Word/Common_Word/Classes/NodeHT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common_Word.Classes
+{
+    class NodeHT
+    {
+        public string Key { get; set; }
+
+        public int Value { get; set; }
+
+        public NodeHT Next { get; set; }
+
+        public NodeHT(string key, int value)
+        {
+            Key = key;
+            Value = value;
+            Next = null;
+        }
+
+        public int getKey()
+        {
+            return Convert.ToInt32(Key);
+        }
+
+        public int getValue()
+        {
+            return Value;
+        }
+    }
+}
