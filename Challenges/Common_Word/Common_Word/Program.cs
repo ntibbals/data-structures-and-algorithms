f3d99b8c23ee36c119e4195fe3f9295f04a4bbcb
ntibbals <ntibbals@gmail.com>
2019-02-24 18:43:53 -0800

    setup env

---
+++ Challenges/Common_Word/Common_Word/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
