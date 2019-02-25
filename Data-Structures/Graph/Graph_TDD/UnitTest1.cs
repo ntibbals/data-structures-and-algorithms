7b83d5a653467ea4d97da86786570e6ee3529613
ntibbals <ntibbals@gmail.com>
2019-02-24 18:24:30 -0800

    working on tests

--- Data-Structures/Graph/Graph_TDD/UnitTest1.cs
+++ Data-Structures/Graph/Graph_TDD/UnitTest1.cs
@@ -20,5 +20,23 @@ namespace Graph_TDD
 
             Assert.Equal(expected, graph.GetVertex());
         }
+
+        [Fact]
+        public void AddEdgeToGraph()
+        {
+            Graphs graph = new Graphs();
+            Vertex testV1 = new Vertex("Test1");
+            Vertex testV2 = new Vertex("Test2");
+
+            graph.AddVertex(testV1);
+            graph.AddVertex(testV2);
+            graph.AddEdge(testV1, testV2, 4);
+            Edge testEdge = new Edge(testV1, testV2, 4);
+            List<Edge> expected = new List<Edge>();
+            expected.Add(testEdge);
+   
+            Assert.Equal(expected, graph.GetNeighbors(testV2));
+
+        }
     }
 }
