4811fd43c5c1ec26cc66ae92e5324a831e456bd6
ntibbals <ntibbals@gmail.com>
2019-02-27 20:39:02 -0800

    setup env

---
+++ Challenges/Depth_First/Depth_First/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Depth_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
